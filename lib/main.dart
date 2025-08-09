b2dcb9795e3f723dabf10d1b9693482c5bd95169
Kaopunnatchaya <boonnak9871@gmail.com>
2025-08-09 15:52:17 +0700

    Step 4 : Sperate the home widget into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
+import 'screen/home.dart';
 void main() {
   //runApp(const MyApp());
 
@@ -21,28 +20,4 @@ void main() {
             ),
             );
 
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //return Text("Hello Flutter! Step2");
-    return Center(
-      child: Container(
-        color: Colors.blueAccent,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(30),
-        child: Text(
-          "Hello Flutter! Sterp3",
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      )
-    );
-  }
 }
